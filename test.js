0e8704b0980fd7cffee6915069bd4249cdfd27d1
Yukimasa Shimada <github@eleos.jp>
2016-10-01 13:02:22 +0000

    mizutさんのソースを参考に元のファイルを残すように変更

--- test.js
+++ test.js
@@ -4,7 +4,13 @@
 	const assert = require('assert');
 	const fs = require('fs');
 	
-	fs.unlink('./tasks.json',(err) => {	// 今ある './tasks.json' ファイルを消します。
+	// 今ある'./tasks.json'をリネームします
+	try {
+		fs.renameSync('./tasks.json', './tasks.json.org');
+	} catch(err) {}
+
+	// fs.unlink('./tasks.json',(err) => {	// ファイルを消す処理が不要に！！
+
 	const todo = require('./index.js');	// 先に読み込むとうまくいかなかったので移動しました。
 	
 	// todo と list のテスト
@@ -24,5 +30,12 @@
 	assert.deepEqual(todo.donelist(), []);
 
 	console.log('テストが正常に完了しました');
-	});
+
+	// './tasks.json'をリネームしなおして元に戻します
+		try {
+	 fs.renameSync('./tasks.json.org', './tasks.json');
+	} catch(err) {}
+
+	// });
+
 })();
