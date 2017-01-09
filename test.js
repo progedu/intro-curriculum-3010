5aaf79c678aadd470ad4fa7c676ad59e60324173
elliot215 <elliot215@gmail.com>
2017-01-09 17:42:53 +0000

    tasks.json を削除してからテストするように修正。

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const assert = require('assert');
 
 const fs = require('fs');
 fs.unlink('./tasks.json', (err) => {
-const todo = require('./index.js');
-    
+    const todo = require('./index.js');
+
     // todo と list のテスト
     todo.todo('ノートを買う');
     todo.todo('鉛筆を買う');
