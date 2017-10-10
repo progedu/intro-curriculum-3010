0aed8d883ac936c2275656e4979425196341987f
gadget-tf <gadget.tf@gmail.com>
2017-10-10 13:44:52 +0000

    練習問題の解答の実装

--- test.js
+++ test.js
@@ -1,21 +1,26 @@
 'use strict';
-const todo = require('./index.js');
-const assert = require('assert');
+const fs = require('fs');
+const fileName = './tasks.json';
 
-// todo と list のテスト
-todo.todo('ノートを買う');
-todo.todo('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+fs.unlink(fileName, (err) => {
+  const todo = require('./index.js');
+  const assert = require('assert');
 
-// done と donelist のテスト
-todo.done('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う']);
-assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+  // todo と list のテスト
+  todo.todo('ノートを買う');
+  todo.todo('鉛筆を買う');
+  assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('鉛筆を買う');
-assert.deepEqual(todo.list(), []);
-assert.deepEqual(todo.donelist(), []);
+  // done と donelist のテスト
+  todo.done('鉛筆を買う');
+  assert.deepEqual(todo.list(), ['ノートを買う']);
+  assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
 
-console.log('テストが正常に完了しました');
+  // del のテスト
+  todo.del('ノートを買う');
+  todo.del('鉛筆を買う');
+  assert.deepEqual(todo.list(), []);
+  assert.deepEqual(todo.donelist(), []);
+
+  console.log('テストが正常に完了しました');
+});
