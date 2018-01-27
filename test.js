5cd03f09e645575bd80fe9d6b375be490b9dde70
sironekotoro <develop@sironekotoro.com>
2018-01-27 06:17:33 +0000

    テスト開始時に既存のtasks.jsonを削除

--- test.js
+++ test.js
@@ -1,21 +1,34 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
 
-// todo と list のテスト
-todo.todo('ノートを買う');
-todo.todo('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+// tasks.jsonファイルがある場合、それを削除する
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
 
-// done と donelist のテスト
-todo.done('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う']);
-assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+    // index.jsが呼ばれた時にtasks.jsonの有無チェックが
+    // 行われるので、ファイルを削除してからindex.jsを
+    // 呼び出す
+    // このため、テスト時には常に
+    // 「./tasks.jsonから復元できませんでした]
+    // と表示される
+    const todo = require('./index.js');
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('鉛筆を買う');
-assert.deepEqual(todo.list(), []);
-assert.deepEqual(todo.donelist(), []);
+    // todo と list のテスト
+    todo.todo('ノートを買う');
+    todo.todo('鉛筆を買う');
+    assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-console.log('テストが正常に完了しました');
+    // done と donelist のテスト
+    todo.done('鉛筆を買う');
+    assert.deepEqual(todo.list(), ['ノートを買う']);
+    assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+
+    // del のテスト
+    todo.del('ノートを買う');
+    todo.del('鉛筆を買う');
+    assert.deepEqual(todo.list(), []);
+    assert.deepEqual(todo.donelist(), []);
+
+    console.log('テストが正常に完了しました');
+
+});
