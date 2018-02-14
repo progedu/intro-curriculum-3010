5953e8ec6a5cff79583160c1d862c342147d76a3
Yoshihiro Kishida <y-kishida@outlook.jp>
2018-02-14 15:59:37 +0000

    ファイルを削除してからテストを実行するように変更

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
 const fs = require('fs');
 
 fs.unlink('./tasks.json', (err) => {
     console.log('ファイルを削除してからテストを実行します');
+    const todo = require('./index.js');
     // todo と list のテスト
     todo.todo('ノートを買う');
     todo.todo('鉛筆を買う');
