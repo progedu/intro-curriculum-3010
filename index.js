89c4a46f9af2be9b92bbb98486286f295f004f93
einSeelen <kumejima14@gmail.com>
2018-08-11 11:42:28 +0000

    例外処理の練習

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const fileName = './tasks.json';
 
 // 同期的にファイルから復元
 try {
-	if (fs.existsSync(fileName)) {
 		const data = fs.readFileSync(fileName, 'utf8');
 		tasks = new Map(JSON.parse(data));
-	}
 } catch (ignore) {
 	console.log(fileName + 'から復元できませんでした');
 }
