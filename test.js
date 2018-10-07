690d15328b146196ec2c374f2b783ce63cc64872
Garacia <samutyan@hotmail.co.jp>
2018-10-07 07:19:38 +0000

    練習問題

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 'use strict';
-const todo = require('./index.js');
-const assert = require('assert');
 
-// todo と list のテスト
-todo.todo('ノートを買う');
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+
+    const todo = require('./index.js');    
+    // todo と list のテスト
+    todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
@@ -19,3 +21,9 @@ assert.deepEqual(todo.list(), []);
 assert.deepEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+
+
+});
+
+const assert = require('assert');
+
