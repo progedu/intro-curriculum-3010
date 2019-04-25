4175356b3a62cbe567d1e7cbe394dc49dd55a5af
chip-cocoa <arkbuchessener1209@icloud.com>
2019-04-25 15:56:31 +0000

    tasks.jsonを消去してから、テストを実行するように変更

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 'use strict';
-const todo = require('./index.js');
+
 const assert = require('assert');
+//テストを実行する前に、tasks.jsonを消去する。
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+    const todo = require('./index.js');
+});
+
 
 // todo と list のテスト
 todo.todo('ノートを買う');
