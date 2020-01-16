9c4b61c53b764a753c4a3a8bacfcba06f3ebff18
Kazuma Murata <kazumario123@gmail.com>
2020-01-17 01:43:55 +0900

    テスト実行前に書き込みファイルを削除する機能を実装

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
+const fs = require('fs');
+fs.unlink('./tasks.json',(err) => {
+  const todo = require('./index.js');
 
-// todo と list のテスト
+  // todo と list のテスト
 todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
@@ -19,3 +21,7 @@ assert.deepEqual(todo.list(), []);
 assert.deepEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+
+});
+
+
