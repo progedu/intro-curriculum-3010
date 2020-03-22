edf0ba19c4fc8143d193ae0f2110752614d0a817
sken <ken.s.rainbow@gmail.com>
2020-03-22 01:17:08 +0000

    exercise completed

--- test.js
+++ test.js
@@ -1,7 +1,31 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
 
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+    const todo = require('./index.js');
+
+    // todo と list のテスト
+    todo.todo('ノートを買う');
+    todo.todo('鉛筆を買う');
+    assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+
+    // done と donelist のテスト
+    todo.done('鉛筆を買う');
+    assert.deepEqual(todo.list(), ['ノートを買う']);
+    assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+
+    // del のテスト
+    todo.del('ノートを買う');
+    todo.del('鉛筆を買う');
+    assert.deepEqual(todo.list(), []);
+    assert.deepEqual(todo.donelist(), []);
+
+    console.log('テストが正常に完了しました');
+
+});
+
+/*
 // todo と list のテスト
 todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
@@ -19,3 +43,4 @@ assert.deepEqual(todo.list(), []);
 assert.deepEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+*/
