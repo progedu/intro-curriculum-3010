a45044df64b54a16b0bed996ab49ad6736c33460
yuppe <nakamura.yutaro78@gmail.com>
2020-04-13 18:26:31 +0900

    asynchoronization

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 'use strict';
-const todo = require('./index.js');
+
 const assert = require('assert');
+
+// テスト前に永続化されているファイルを消す
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+    const todo = require('./index.js');
+});
 
 // todo と list のテスト
 todo.todo('ノートを買う');
