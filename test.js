9dec2af8d24f32ee7194ad848173233656725f6d
444aaa <nn.contact.n@gmail.com>
2020-06-10 13:39:13 +0900

    tasksを削除してからtestするように変更

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 'use strict';
-const todo = require('./index.js');
+const fs = require('fs')
 const assert = require('assert');
 
-// todo と list のテスト
-todo.todo('ノートを買う');
-todo.todo('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+fs.unlink('./tasks.json', (err) =>{
+	console.log('./tasks.json を削除しました。')
+	
+	// todo と list のテスト
+	const todo = require('./index.js');
+	todo.todo('ノートを買う');
+	todo.todo('鉛筆を買う');
+	assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
 // done と donelist のテスト
 todo.done('鉛筆を買う');
@@ -19,3 +23,4 @@ assert.deepEqual(todo.list(), []);
 assert.deepEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+})	
