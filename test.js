5be6ff053cc2acd1027483a85953339614d5cfa1
Makino Yudi <hymakino@icloud.com>
2020-07-04 00:36:04 +0900

    テストを実施するときにtasks.jsonを削除する処理を実装した

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 'use strict';
 const todo = require('./index.js');
 const assert = require('assert');
+const fs = require('fs');
+
+fs.unlink('./tasks.json', err => {
 
 // todo と list のテスト
 todo.todo('ノートを買う');
@@ -17,5 +20,6 @@ todo.del('ノートを買う');
 todo.del('鉛筆を買う');
 assert.deepEqual(todo.list(), []);
 assert.deepEqual(todo.donelist(), []);
+});
 
 console.log('テストが正常に完了しました');
