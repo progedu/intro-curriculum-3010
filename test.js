72d0cb81c1f6aa32f7fd9526581a4e6dd0c4e099
kensuke <t-kensuke@hotmail.com>
2020-07-04 21:56:08 +0900

    unlink関数追加

--- test.js
+++ test.js
@@ -1,21 +1,25 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
 
-// todo と list のテスト
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+    const todo = require('./index.js');
+
+// todoとlistのテスト
 todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-// done と donelist のテスト
+// doneとdonelistのテスト
 todo.done('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う']);
+assert.deepEqual(todo.list(), ['ノートを買う'],);
 assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
 
-// del のテスト
+// delのテスト
 todo.del('ノートを買う');
 todo.del('鉛筆を買う');
 assert.deepEqual(todo.list(), []);
 assert.deepEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+});
