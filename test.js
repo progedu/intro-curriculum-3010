29be6d2423d948f73f731661e2e97c50c069b537
zgmf515 <rawwle@yahoo.co.jp>
2020-07-10 17:04:36 +0900

    ファイルを削除してからテストを実行するように test.js を修正

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
+
+const fs = require('fs');
+fs.unlink('./tasks.json', (err) => {
+  // テスト処理
+  const todo = require('./index.js');
+});
+
 
 // todo と list のテスト
 todo.todo('ノートを買う');
