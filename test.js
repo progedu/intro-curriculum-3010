e6e7d9f15574bb4c83eb9906e8af9c404441c57e
水道橋で夜更かし <tomo_952@yahoo.co.jp>
2021-01-22 22:10:03 +0900

    第3章11節課題:データ永続化の改善

--- test.js
+++ test.js
@@ -2,20 +2,24 @@
 const todo = require('./index.js');
 const assert = require('assert');
 
-// add と list のテスト
-todo.add('ノートを買う');
-todo.add('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+const fs = require('fs');
+fs.unlink('./tasks.json', err => {
 
-// done と donelist のテスト
-todo.done('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), ['ノートを買う']);
-assert.deepStrictEqual(todo.donelist(), ['鉛筆を買う']);
+  // todo と list のテスト
+  todo.todo('ノートを買う');
+  todo.todo('鉛筆を買う');
+  assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), []);
-assert.deepStrictEqual(todo.donelist(), []);
+  // done と donelist のテスト
+  todo.done('鉛筆を買う');
+  assert.deepEqual(todo.list(), ['ノートを買う']);
+  assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
 
-console.log('テストが正常に完了しました');
+  // del のテスト
+  todo.del('ノートを買う');
+  todo.del('鉛筆を買う');
+  assert.deepEqual(todo.list(), []);
+  assert.deepEqual(todo.donelist(), []);
+
+  console.log('テストが正常に完了しました');
+});
