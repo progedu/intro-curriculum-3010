035235e32f23a7823d31ad6bba032514e60f6703
Kanaka Ishida <kanapikotan@icloud.com>
2021-02-20 17:00:44 +0900

    ファイルを削除してからテストを実行するように修正

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
 
-// add と list のテスト
+//テストの前に永続化されているファイルを消す
+const fs = require('fs');
+fs.unlink('./tasks.json', err => {
+  const todo = require('./index.js');
+
+  // add と list のテスト
 todo.add('ノートを買う');
 todo.add('鉛筆を買う');
 assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
@@ -19,3 +23,5 @@ assert.deepStrictEqual(todo.list(), []);
 assert.deepStrictEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+
+});
