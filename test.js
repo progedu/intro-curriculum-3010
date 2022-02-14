be9699418546214719cfb6b67fc9fc078b6293fb
yasu-ryo <yasuryo.workmail@gmail.com>
2022-02-14 15:16:23 +0900

    ファイルを削除してからテストを実行するように test.js を修正しました。

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
+
+// テストの前に永続化されているファイルを消す
+const fs = require('assert');
+fs.unlink('./tasks.json', err => {
+    const todo = require('./index.js');
 
 // add と list のテスト
 todo.add('ノートを買う');
@@ -19,3 +23,6 @@ assert.deepStrictEqual(todo.list(), []);
 assert.deepStrictEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+
+});
+
